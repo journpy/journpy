4edda1d03fb56edef11ffd73ca66999710b4ea7b
Uchenna Adubasim <uchadubs@yahoo.com>
2023-06-05 09:09:30 +0100

    Postgres

--- journpy/settings.py
+++ journpy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6ppm_ri8037@&*t=png5*m#u%g1z_n928w1#sd5)n-x=3=7(+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://journpy.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 
@@ -82,10 +82,20 @@ WSGI_APPLICATION = 'journpy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'journpydb',
+        'USER': 'postgres',
+        'PASSWORD': '1234Uche',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
