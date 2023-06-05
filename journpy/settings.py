c4c2210cfab788e25fe78c7acce2fb7d8ec14fed
Uchenna Adubasim <uchadubs@yahoo.com>
2023-06-05 11:36:02 +0100

    Deployed to Heroku using PostgreSQL.

--- journpy/settings.py
+++ journpy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 ######################
 import dj_database_url
-#######################
+######################
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,12 +84,6 @@ WSGI_APPLICATION = 'journpy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -100,7 +94,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-###################################################
+######################################################
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 ######################################################
@@ -150,5 +144,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'users:login'
 
 # Configure Django App for Heroku.
+import os
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
